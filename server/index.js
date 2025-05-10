dfc9218e7927fcd779597e940d2ffbcec40b6fbc
Sajal Garg <221210093@nitdelhi.ac.in>
2025-05-10 15:56:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -22,7 +22,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://acquireworld-frontend.onrender.com",
     credentials:true
 }));
  
